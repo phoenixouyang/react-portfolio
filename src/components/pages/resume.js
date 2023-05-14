8b2b579c1651bdfb862d6d85442ec3f163ab4ecf
Pholobo <ouyangphoenix@gmail.com>
2023-05-14 15:40:01 -0400

    adds terminal icons and css styling

--- src/components/pages/resume.js
+++ src/components/pages/resume.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircle } from '@fortawesome/free-regular-svg-icons';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
+import '../../styles/style.css'
 
 export default function Resume() {
   return (
     <container className="page-container">
-      <div className="about-terminal">
+      <div className="about-terminal terminal">
         <div className="terminal-bar">
           <FontAwesomeIcon icon={faCircle} id="terminal-red"/>
           <FontAwesomeIcon icon={faCircle} id="terminal-yellow"/>
@@ -16,9 +17,11 @@ export default function Resume() {
         </div>
         
       </div>
-      <div className="skills-terminal">
+      <div className="skills-terminal terminal">
         <div className="terminal-bar">
-
+          <FontAwesomeIcon icon={faCircle} id="terminal-red"/>
+          <FontAwesomeIcon icon={faCircle} id="terminal-yellow"/>
+          <FontAwesomeIcon icon={faCircle} id="terminal-green"/>
         </div>
         <div className="terminal-screen">
 
